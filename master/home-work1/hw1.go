b2a4d900aeee21f0796cf2e8109d2521c206fdae
Александр <aleksandr@MacBook-Pro-Aleksandr.local>
2020-07-14 20:28:05 +0300

    first commit

--- master/home-work1/hw1.go
+++ master/home-work1/hw1.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
+const a = 3
+const b = 5
+
+var c float64
+var summa = new(float64)
+
 func main() {
-	$END$
+	c = math.Sqrt(a*a + b*b)
+	fmt.Println("Гипотенуза треугольника = ", c)
+	fmt.Println("Площадь треугольника = ", a*b)
+	fmt.Println("Периметр треугольника = ", a+b+c)
+
+	fmt.Println("Введите сумму вклада")
+	fmt.Scanln(&summa)
 }
