5822231f6f603bd04b8f0ad6fd0ede8f712f30bc
Александр <aleksandr@MacBook-Pro-Aleksandr.local>
2020-07-16 19:48:10 +0300

    first commit

--- master/home-work1/hw1.go
+++ master/home-work1/hw1.go
@@ -11,8 +11,10 @@ const kurs = 71
 
 var c float64
 var summa = new(float64)
+var percent float64
 
 func main() {
+
 	fmt.Println("Введите сумму в рублях для конвертации в доллары")
 	fmt.Scanln(summa)
 	fmt.Println("Вы можете приобрести", *summa/kurs)
@@ -24,5 +26,11 @@ func main() {
 
 	fmt.Println("Введите сумму вклада в рублях")
 	fmt.Scanln(summa)
-	fmt.Println(*summa)
+	fmt.Println("Введите значение годового процента")
+	fmt.Scanln(&percent)
+
+	for i := 0; i < 5; i++ {
+		*summa = *summa + *summa*(percent/100)
+	}
+	fmt.Println("Через пять лет сумма будет равна ", *summa)
 }
